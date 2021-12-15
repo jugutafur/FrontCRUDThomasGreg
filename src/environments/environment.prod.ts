e5d62198599d11b650bc8c311fb3a53c502e51d3
DESKTOP-8GLU08G\HP <juantafur.25@gmail.com>
2021-12-15 16:29:16 -0500

    add new features

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
 
   Url_apiMarvel : 'https://gateway.marvel.com/v1/public/characters?ts=1&apikey=d165ebdf45a5d723c1ad698d3db167c1&hash=69d5b299962d000cb0326f675e749f86',
-  Url_apiBD : 'https://back-thomasgreg-crud.herokuapp.com/api/User/'
+  Url_apiBD : 'https://back-thomasgreg-crud.herokuapp.com/api/consulta/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
 
   Url_apiMarvel : 'https://gateway.marvel.com/v1/public/characters?ts=1&apikey=d165ebdf45a5d723c1ad698d3db167c1&hash=69d5b299962d000cb0326f675e749f86',
-  Url_apiBD : 'https://back-thomasgreg-crud.herokuapp.com/api/User/'
+  Url_apiBD : 'https://back-thomasgreg-crud.herokuapp.com/api/consulta/'
 };
 
 /*
